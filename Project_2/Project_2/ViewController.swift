4727184c949e7ecec22ec1249cb9c651f4cadde2
NickYao <zican.yao@gmail.com>
2016-12-11 18:54:10 +0800

    add Project_2

---
+++ Project_2/Project_2/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Project_2
+//
+//  Created by yaozican on 2016/12/11.
+//  Copyright © 2016年 NICKYAO. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+   
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button2.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button3.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
+        
+    }
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        var title: String
+
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "your score is \(score)", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "continue", style: .Default, handler: askQuestion))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+        
+        button1.setImage(UIImage(named:countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named:countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named:countries[2]), forState: .Normal)
+   
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        title = countries[correctAnswer].uppercaseString
+    
+    }
+    
+  
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
